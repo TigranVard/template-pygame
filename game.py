ced04e6870af78c777e5afb3c5f3885d019a953d
TigranVard <146510780+TigranVard@users.noreply.github.com>
2024-01-27 09:46:33 +0300

    Add files via upload

--- game.py
+++ game.py
@@ -1,73 +1,84 @@
 import pygame
 import random
 
+# Переменные с размером экрана
 WIDTH = 500
 HEIGHT = 500
-FPS = 500
-# # Задаем цвета
-# WHITE = (255,255,255)
-# BLACK = (255,255,255)
-# GREEN = (0,255,0)
-# RED = (0,0,255)
-# BLUE = (0,0,255)
+
+FPS = 999
+# Задаем цвета
+WHITE = (255,255,255)
+BLACK = (255,255,255)
+GREEN = (0,255,0)
+RED = (0,0,255)
+BLUE = (0,0,255)
+# создаем игру и окно
 pygame.init()
 pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My game")
-clock = pygame.time.Clock()
-running = True
-red = 0
-green = 0
-black = 0
-blue = 0
-isRedMax = False
 
+# создаем переменную, в которую запихиваем экран, устонавливаем ширину и высоту
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# название игры
+pygame.display.set_caption("My game")
+# переменная, которая хранит время обновления экрана
+clock = pygame.time.Clock()
+# цикл игры
+running = True
+# цвет фона
+screenColor = (0,0,0)
+
+# параметры игрока
 y = 0
 x = 0
+# ширина и высота игрока
+width_player = 25
+height_player = 25
 
+# цвет яблока
 colorApple = (255, 0, 50)
+# координаты яблока
 x_apple = 100
 y_apple = 200
 
+#цикл
 while running:
+    #держим цикл на правильной скорости
     clock.tick(FPS)
-    screen.fill((red,green,blue))
+    # заполнять экран цветом
+    screen.fill(screenColor)
 
-    # if isRedMax == True:
-    #     green = green + 0.5
-    # else:
-    #     red = red + 0.5
-    #     if red == 255:
-    #         red = 0
-    #         isRedMax = True
-    #     print(red)
+    # рисуем прямоугольный прямоугольник который квадрат  с цветом координатами и размером
+    pygame.draw.rect(screen,(100,90,200),[x,y,width_player,height_player])
+
+    # рисуем яблоко 15 - это R радиус
+    pygame.draw.circle(screen,colorApple,[x_apple,y_apple],10)
 
 
-    pygame.draw.rect(screen,(100,90,200),[x,y,20,20])
-    #рисуем яблоко
-    pygame.draw.circle(screen,colorApple,[x_apple,y_apple],20)
+    if (x<0 or x > WIDTH - width_player):
+        running = False
+        pass
+    if ((x < x_apple+15 and x_apple-15 < x + width_player) and (y < y_apple+15 and y_apple-15 < y + height_player)):
+        width_player += 10
+        x_apple = random.randint(0,WIDTH)
+        y_apple = random.randint(0,HEIGHT)
+        pygame.draw.circle(screen,colorApple,[x_apple,y_apple],10)
 
-    for event in  pygame.event.get():
+    
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        y = y - 0.25
+    if keys[pygame.K_s]:
+        y = y + 0.25
+    if keys[pygame.K_a]:
+        x = x - 0.25
+    if keys[pygame.K_d]:
+        x = x + 0.25
+
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                y = y - 5
-                print("Нажали на W")
-            if event.key == pygame.K_s:
-                y = y + 5
-                print("Нажали на S")
-            if event.key == pygame.K_a:
-                x = x - 5
-                print("Нажали на A")
-            if event.key == pygame.K_d:
-                x = x + 5
-                print("Нажали на D")
-            if event.key == pygame.K_SPACE:
-                print("Нажали на SPACE")
-        
 
-        
     
     pygame.display.flip()
 pygame.quit()
