9ea7ea703bf25e405c142e3b3de49a077d3d4fe0
TigranVard <146510780+TigranVard@users.noreply.github.com>
2024-03-23 16:24:59 +1000

    Add files via upload

--- zadachi.py
+++ zadachi.py
@@ -22,15 +22,33 @@
 #         s = s + n
 # print(s)
 
-pi = 3
+# pi = 3
 
-a = 2
-for i in range(1,16):
+# a = 2
+# for i in range(1,16):
     
-    if i%2 == 0:
-        pi = pi - 4/(a*(a+1)*(a+2))
-    else:
-        pi = pi + 4/(a*(a+1)*(a+2))
+#     if i%2 == 0:
+#         pi = pi - 4/(a*(a+1)*(a+2))
+#     else:
+#         pi = pi + 4/(a*(a+1)*(a+2))
 
-    a += 2
-print(pi)
+#     a += 2
+# print(pi)
+
+
+
+
+
+def stringReplace(string):
+   string2 = ""
+   for i in range(0,len(string)):
+      if string[i] == "a":
+         string2 += "G"
+      elif string[i-1] == " " or i == 0:
+         string2 += string[i].capitalize()
+      else:
+         string2 += string[i]
+   print(string2)
+
+
+stringReplace("Я ОЧЕНЬ ХОЧУ ДОМОЙ HELP!!!!")
